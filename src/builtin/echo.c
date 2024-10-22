2f12914697a8cdca513f9d146ac1cd8e556d6cc2
Elias <eli_elabed@hotmail.fr>
2024-10-22 17:21:09 +0200

    echo implemented

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,14 +6,31 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:53:18 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/10/22 16:16:10 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:19:12 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void echo_builtin(char **args)
+void	echo_builtin(char **args)
 {
-    (void)args;
-	printf("echo command executed\n");
+	int		i;
+	bool	newline;
+
+	i = 1;
+	newline = true;
+	if (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+	{
+		newline = false;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
 }
