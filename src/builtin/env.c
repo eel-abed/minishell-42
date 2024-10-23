7fb8b49761ed2a9fd12e637970797318a8924266
Elias <eli_elabed@hotmail.fr>
2024-10-23 16:09:53 +0200

    env builtin

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,13 +6,21 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:53:40 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/10/22 16:18:08 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:05:25 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void env_builtin(void)
+void	env_builtin(void)
 {
-	printf("env command executed\n");
+	extern char	**environ;
+	char		**env;
+
+	env = environ;
+	while (*env)
+	{
+		ft_putendl_fd(*env, STDOUT_FILENO);
+		env++;
+	}
 }
