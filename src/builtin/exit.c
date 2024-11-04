2ca7faedb36bf716eeeae4fd2721f5e9126dd412
Elias <eli_elabed@hotmail.fr>
2024-11-04 19:25:47 +0100

    exit and unset

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,27 +6,37 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:53:56 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/10/22 16:16:17 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/11/04 19:21:36 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void exit_builtin(char **args)
-{
-    (void)args;
-	printf("exit command executed\n");
-}
-
-/*
 void	exit_builtin(char **args)
 {
-	if (args[1])
+	int			exit_status;
+	char		*endptr;
+	long		status;
+	const char	*error_msg;
+
+	if (args[1] == NULL)
 	{
-		printf("minishell: exit: too many arguments\n");
-		return;
+		exit_status = 0;
 	}
-	printf("exit\n");
-	exit(g_exit_status);
+	else
+	{
+		status = strtol(args[1], &endptr, 10);
+		if (*endptr != '\0' || endptr == args[1] || status < 0 || status > 255)
+		{
+			error_msg = "minishell: exit: numeric argument required\n";
+			write(STDERR_FILENO, error_msg, strlen(error_msg));
+			exit_status = 1;
+		}
+		else
+		{
+			exit_status = (int)(status % 256);
+		}
+	}
+	write(STDOUT_FILENO, "exit\n", 6);
+	exit(exit_status);
 }
-*/
--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,32 +6,33 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:54:48 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/11/04 17:31:13 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/11/04 19:25:26 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void unset_builtin(char **args)
+void	unset_builtin(char **args)
 {
-	int i = 1;
-	int j;
-	extern char **environ;
+	int			i;
+	int			j;
+	extern char	**environ;
 
+	i = 1;
 	while (args[i])
 	{
 		j = 0;
-
 		while (environ[j])
 		{
-			if (strncmp(environ[j], args[i], strlen(args[i])) == 0 && environ[j][strlen(args[i])] == '=')
+			if (strncmp(environ[j], args[i], strlen(args[i])) == 0
+				&& environ[j][strlen(args[i])] == '=')
 			{
 				while (environ[j])
 				{
 					environ[j] = environ[j + 1];
 					j++;
 				}
-				break;
+				break ;
 			}
 			j++;
 		}
