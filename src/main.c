9fc77a45da191f28b581b5294007c5e8f8669bd7
Makesense22pls <maxencefournierr@gmail.com>
2025-02-08 05:23:02 +0100

    oe

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mafourni <mafourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 14:06:29 by maxencefour       #+#    #+#             */
-/*   Updated: 2025/02/07 15:19:51 by mafourni         ###   ########.fr       */
+/*   Updated: 2025/02/08 01:38:17 by mafourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int g_exit_status;
 
 int main(int argc, char **argv, char **envp) 
 {
-    t_list  token_clean;
+    t_list  *token_clean;
     char *input;
     t_env *env;
     t_command cmd;
@@ -32,19 +32,21 @@ int main(int argc, char **argv, char **envp)
     setup_signals();
     ft_memset(&cmd, 0, sizeof(t_command));
     cmd.env = env;
-
-    while (1) {
+    while (1) 
+    {
         input = readline("minishell> ");
         if (!input)
             break;
-
         if (strlen(input) > 0) 
         {
             add_history(input);
-            token_clean = ft_lexer();
+            token_clean = ft_lexer(input);
+            if(token_clean == NULL)
+                (printf("CHECH\n"));
+            // printf("Tu le sais %d \n",token_clean->index);
             // char **args = ft_split(input, ' ');
-            cmd.cmd1 = NULL;
-            cmd.cmd2 = NULL;
+            // cmd.cmd1 = NULL;
+            // cmd.cmd2 = NULL;
             // execute_command(args, &cmd);
             // // Add cleanup for args
             // int i = 0;
