bd630170f022d6ac934387f3ca4c806008a84a37
eel-abed <eli_elabed@hotmail.fr>
2025-02-21 17:14:17 +0100

    Refactor command execution to use token splitting for arguments

--- src/execution/exec_cmd_ext.c
+++ src/execution/exec_cmd_ext.c
@@ -6,7 +6,7 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:33:27 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/02/20 19:40:42 by eel-abed         ###   ########.fr       */
+/*   Updated: 2025/02/21 17:13:31 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,77 +72,66 @@ char	*find_command_path(char *cmd, t_env *env)
 
 int execute_external_command(t_tokens *tokens, t_command *cmd_info)
 {
-	char *cmd_path;
-	char **env_array;
-	char **cmd_args;
-	int exit_status;
-	pid_t pid;
-	int status;
+    char *cmd_path;
+    char **env_array;
+    char **cmd_args;
+    int exit_status;
+    pid_t pid;
+    int status;
 
-	cmd_args = malloc(sizeof(char *) * (mini_lstsize(tokens) + 1));
-	if (!cmd_args)
-		return (1);
+    // Split the token value into command and arguments
+    cmd_args = ft_split(tokens->value, ' ');
+    if (!cmd_args)
+        return (1);
 
-	t_tokens *current = tokens;
-	int i = 0;
-	while (current)
-	{
-		cmd_args[i] = current->value;
-		current = current->next;
-		i++;
-	}
-	cmd_args[i] = NULL;
+    cmd_path = find_command_path(cmd_args[0], cmd_info->env);
+    if (!cmd_path)
+    {
+        write(2, cmd_args[0], ft_strlen(cmd_args[0]));
+        write(2, ": command not found\n", 20);
+        free_paths(cmd_args);
+        return (127);
+    }
 
-	cmd_path = find_command_path(cmd_args[0], cmd_info->env);
-	if (!cmd_path)
-	{
-		write(2, cmd_args[0], ft_strlen(cmd_args[0]));
-		write(2, ": command not found\n", 20);
-		free(cmd_args);
-		return (127);
-	}
+    env_array = env_to_array(cmd_info->env);
+    if (!env_array)
+    {
+        free(cmd_path);
+        free_paths(cmd_args);
+        return (1);
+    }
 
-	env_array = env_to_array(cmd_info->env);
-	if (!env_array)
-	{
-		free(cmd_path);
-		free(cmd_args);
-		return (1);
-	}
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(cmd_path);
+        free_paths(env_array);
+        free_paths(cmd_args);
+        return (1);
+    }
 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(cmd_path);
-		free_paths(env_array);
-		free(cmd_args);
-		return (1);
-	}
+    if (pid == 0)  // Child process
+    {
+        execve(cmd_path, cmd_args, env_array);
+        perror(cmd_args[0]);
+        free(cmd_path);
+        free_paths(env_array);
+        free_paths(cmd_args);
+        exit(126);
+    }
+    else  // Parent process
+    {
+        waitpid(pid, &status, 0);
+        free(cmd_path);
+        free_paths(env_array);
+        free_paths(cmd_args);
 
-	if (pid == 0)  // Child process
-	{
-		execve(cmd_path, cmd_args, env_array);
-		
-		perror(cmd_args[0]);
-		free(cmd_path);
-		free_paths(env_array);
-		free(cmd_args);
-		exit(126);
-	}
-	else  // Parent process
-	{
-		waitpid(pid, &status, 0);
-		
-		free(cmd_path);
-		free_paths(env_array);
-		free(cmd_args);
+        if (WIFEXITED(status))
+            exit_status = WEXITSTATUS(status);
+        else
+            exit_status = 1;
 
-		if (WIFEXITED(status))
-			exit_status = WEXITSTATUS(status);
-		else
-			exit_status = 1;
-
-		return (exit_status);
-	}
+        return (exit_status);
+    }
 }
