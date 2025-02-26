c7376343cc0a201c8e6d3c5e777ebd8ee24c873c
eel-abed <eli_elabed@hotmail.fr>
2025-02-26 18:27:58 +0100

    Refactor piped command execution to handle multiple commands and improve memory management

--- src/helper_func/pipe.c
+++ src/helper_func/pipe.c
@@ -6,7 +6,7 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:18:41 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/02/26 17:40:08 by eel-abed         ###   ########.fr       */
+/*   Updated: 2025/02/26 18:14:22 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,94 +14,117 @@
 
 void execute_piped_commands(t_tokens *tokens, t_command *cmd_info, t_garbage **gc)
 {
-    t_tokens *cmd1;
-    t_tokens *cmd2;
-    pid_t pid1, pid2;
-    int pipefd[2];
-    int status1, status2;
-    
-    // Find the pipe token
-    cmd1 = tokens;
-    cmd2 = NULL;
-    
-    while (tokens)
+    int cmd_count = 1;
+    int pipe_count;
+    int **pipes;
+    pid_t *pids;
+    t_tokens *current = tokens;
+    int i, status;
+
+    // Count the number of commands (number of pipes + 1)
+    while (current)
     {
-        if (tokens->type == kind_pipe)
+        if (current->type == kind_pipe)
+            cmd_count++;
+        current = current->next;
+    }
+    
+    pipe_count = cmd_count - 1;
+    
+    // Allocate memory for pipes and pids
+    pipes = gc_malloc(gc, sizeof(int*) * pipe_count);
+    for (i = 0; i < pipe_count; i++)
+        pipes[i] = gc_malloc(gc, sizeof(int) * 2);
+    pids = gc_malloc(gc, sizeof(pid_t) * cmd_count);
+    
+    if (!pipes || !pids)
+    {
+        ft_putstr_fd("minishell: memory allocation error\n", 2);
+        return;
+    }
+    
+    // Create all pipes
+    for (i = 0; i < pipe_count; i++)
+    {
+        if (pipe(pipes[i]) < 0)
         {
-            cmd2 = tokens->next;
-            break;
+            perror("pipe");
+            return;
         }
-        tokens = tokens->next;
     }
     
-    if (!cmd2)
-        return; // Error: no second command after pipe
-    
-    // Create pipe
-    if (pipe(pipefd) == -1)
+    // Execute each command
+    current = tokens;
+    for (i = 0; i < cmd_count; i++)
     {
-        perror("pipe");
-        cmd_info->exit_status = 1;
-        return;
-    }
-    
-    // First command (writes to pipe)
-    pid1 = fork();
-    if (pid1 < 0)
-    {
-        perror("fork");
-        cmd_info->exit_status = 1;
-        return;
-    }
-    
-    if (pid1 == 0) // Child process for first command
-    {
-        // Close read end of pipe
-        close(pipefd[0]);
+        // Skip pipe tokens to get to the next command
+        if (i > 0)
+        {
+            while (current && current->type != kind_pipe)
+                current = current->next;
+            if (current)
+                current = current->next;
+        }
         
-        // Redirect stdout to pipe write end
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
+        if (!current)
+            break;
         
-        // Execute first command
-        execute_command(cmd1, cmd_info, gc);
-        exit(cmd_info->exit_status);
+        pids[i] = fork();
+        if (pids[i] < 0)
+        {
+            perror("fork");
+            return;
+        }
+        
+        if (pids[i] == 0) // Child process
+        {
+            // Set up pipe redirections
+            
+            // If not first command, set stdin to read from previous pipe
+            if (i > 0)
+            {
+                dup2(pipes[i-1][0], STDIN_FILENO);
+            }
+            
+            // If not last command, set stdout to write to next pipe
+            if (i < pipe_count)
+            {
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+            
+            // Close all pipe fds in child
+            for (int j = 0; j < pipe_count; j++)
+            {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+            
+            // Execute the command
+            execute_command(current, cmd_info, gc);
+            exit(cmd_info->exit_status);
+        }
     }
     
-    // Second command (reads from pipe)
-    pid2 = fork();
-    if (pid2 < 0)
+    // Close all pipe fds in parent
+    for (i = 0; i < pipe_count; i++)
     {
-        perror("fork");
-        cmd_info->exit_status = 1;
-        return;
+        close(pipes[i][0]);
+        close(pipes[i][1]);
     }
     
-    if (pid2 == 0) // Child process for second command
+    // Wait for all children
+    for (i = 0; i < cmd_count; i++)
     {
-        // Close write end of pipe
-        close(pipefd[1]);
+        waitpid(pids[i], &status, 0);
         
-        // Redirect stdin to pipe read end
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-        
-        // Execute second command
-        execute_command(cmd2, cmd_info, gc);
-        exit(cmd_info->exit_status);
+        // The exit status of the last command is the exit status
+        // of the whole pipeline
+        if (i == cmd_count - 1)
+        {
+            if (WIFEXITED(status))
+                cmd_info->exit_status = WEXITSTATUS(status);
+            else if (WIFSIGNALED(status))
+                cmd_info->exit_status = 128 + WTERMSIG(status);
+        }
     }
-    
-    // Parent process
-    close(pipefd[0]);
-    close(pipefd[1]);
-    
-    // Wait for both commands to complete
-    waitpid(pid1, &status1, 0);
-    waitpid(pid2, &status2, 0);
-    
-    // Set exit status to the status of the second command
-    if (WIFEXITED(status2))
-        cmd_info->exit_status = WEXITSTATUS(status2);
-    else
-        cmd_info->exit_status = 1;
 }
