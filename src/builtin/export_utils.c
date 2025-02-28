e693278fa1461616261c7b5a32705722dc3b3e9f
eel-abed <eli_elabed@hotmail.fr>
2025-02-28 18:02:34 +0100

    refactor: improve exit, export, and unset built-in commands for better error handling and code clarity ( norminette criss)

--- src/builtin/export_utils.c
+++ src/builtin/export_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mafourni <mafourni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:50:47 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/02/25 18:08:12 by mafourni         ###   ########.fr       */
+/*   Updated: 2025/02/28 17:45:03 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ void	append_env_var(t_env *env, t_env_var *new_var)
 	current->next = new_var;
 }
 
-void	handle_export_error(char *arg,t_garbage **gc)
+void	handle_export_error(char *arg, t_garbage **gc)
 {
 	char	*error_msg;
 	char	*temp;
 
-	error_msg = ft_strjoin("minishell: export: '", arg,gc);
+	error_msg = ft_strjoin("minishell: export: '", arg, gc);
 	temp = error_msg;
-	error_msg = ft_strjoin(error_msg, "': not a valid identifier",gc);
+	error_msg = ft_strjoin(error_msg, "': not a valid identifier", gc);
 	ft_putendl_fd(error_msg, STDERR_FILENO);
 }
