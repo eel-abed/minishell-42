40cd11c2f74beef3cd09f4c0448116c7d2f47db6
eel-abed <eli_elabed@hotmail.fr>
2025-02-28 18:10:37 +0100

    norminette echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mafourni <mafourni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:53:18 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/02/25 15:23:31 by mafourni         ###   ########.fr       */
+/*   Updated: 2025/02/28 18:07:18 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ static bool	is_valid_n_flag(const char *str)
 {
 	int	i;
 
-	
 	if (!str || str[0] != '-')
 		return (false);
 	i = 1;
@@ -27,6 +26,19 @@ static bool	is_valid_n_flag(const char *str)
 		i++;
 	}
 	return (i > 1);
+}
+
+static void print_echo_args(char **args, int i, bool newline)
+{
+    while (args[i])
+    {
+        write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
+        if (args[i + 1])
+            write(STDOUT_FILENO, " ", 1);
+        i++;
+    }
+    if (newline)
+        write(STDOUT_FILENO, "\n", 1);
 }
 
 void	echo_builtin(char **args)
@@ -41,51 +53,29 @@ void	echo_builtin(char **args)
 		newline = false;
 		i++;
 	}
-	while (args[i])
-	{
-		write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
-		if (args[i + 1])
-			write(STDOUT_FILENO, " ", 1);
-		i++;
-	}
-	if (newline)
-		write(STDOUT_FILENO, "\n", 1);
+	print_echo_args(args, i, newline);
 }
 
-void echo_builtin_tokens(t_tokens *tokens,t_garbage **gc)
+void	echo_builtin_tokens(t_tokens *tokens, t_garbage **gc)
 {
-    bool    newline;
-    char    **args;
-    int     i;
-    char    *cmd;
+	bool	newline;
+	char	**args;
+	int		i;
+	char	*cmd;
 
-    args = ft_split(tokens->value, ' ',gc);
-    if (!args)
-        return;
-
-    cmd = args[0];
-    if (ft_strncmp(cmd, "echo", 4) == 0)
-        i = 1;  // Skip "echo" command
-    else
-        i = 0;  // Start from first argument
-    newline = true;
-
-    while (args[i] && is_valid_n_flag(args[i]))
-    {
-        newline = false;
-        i++;
-    }
-    
-    while (args[i])
-    {
-        write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
-        if (args[i + 1])
-            write(STDOUT_FILENO, " ", 1);
-        i++;
-    }
-
-    if (newline)
-        write(STDOUT_FILENO, "\n", 1);
-
-    // free_paths(args);
+	args = ft_split(tokens->value, ' ', gc);
+	if (!args)
+		return ;
+	cmd = args[0];
+	if (ft_strncmp(cmd, "echo", 4) == 0)
+		i = 1;
+	else
+		i = 0;
+	newline = true;
+	while (args[i] && is_valid_n_flag(args[i]))
+	{
+		newline = false;
+		i++;
+	}
+	print_echo_args(args, i, newline);
 }
