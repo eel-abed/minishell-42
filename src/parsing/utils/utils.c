e85bcb11a40b4d72f3d694609c695e07c65d7319
Makesense22pls <maxencefournierr@gmail.com>
2025-03-12 13:10:39 +0100

    echo_space_empty

--- src/parsing/utils/utils.c
+++ src/parsing/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: mafourni <mafourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 17:50:05 by mafourni          #+#    #+#             */
-/*   Updated: 2025/03/12 11:42:46 by mafourni         ###   ########.fr       */
+/*   Updated: 2025/03/12 13:09:48 by mafourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,10 +77,10 @@ void	print_tokens(t_tokens *list)
 	current = list;
 	while (list)
 	{
-		printf("Token[%d]:\n", i);
-		printf("  Value: [%s]\n", list->value);
-		printf("  Type:  [%d]\n", list->type);
-		printf("------------------------\n");
+		// printf("Token[%d]:\n", i);
+		// printf("  Value: [%s]\n", list->value);
+		// printf("  Type:  [%d]\n", list->type);
+		// printf("------------------------\n");
 		list = list->next;
 		i++;
 	}
