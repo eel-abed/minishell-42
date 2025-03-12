61808b5958fad74da7a635fb56a2733445cd69c0
Vincent Da Conceicao Ramos <vramos@equancy.cloud>
2025-03-12 13:37:09 +0100

    fixed sigquit

--- src/helper_func/signals.c
+++ src/helper_func/signals.c
@@ -44,11 +44,21 @@ void handle_sigquit(int sig)
 void	setup_signals(void)
 {
 	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+	struct sigaction	sa_tstp;
 
 	sa_int.sa_handler = handle_sigint;
 	sigemptyset(&sa_int.sa_mask);
 	sa_int.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa_int, NULL);
+	sa_quit.sa_handler = SIG_IGN;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+	sa_tstp.sa_handler = SIG_IGN;
+	sigemptyset(&sa_tstp.sa_mask);
+	sa_tstp.sa_flags = SA_RESTART;
+	sigaction(SIGTSTP, &sa_tstp, NULL);
 }
 
 void	setup_here_doc_signals(void)
